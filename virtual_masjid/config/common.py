f298bd09c8ccfe5237dd793451c84952b50f1173
saidkassim <saidkassim@bitbucket.org>
2020-04-29 13:42:56 +0100

    added whitenoise to collectstatic in heroku

--- virtual_masjid/config/common.py
+++ virtual_masjid/config/common.py
@@ -46,6 +46,7 @@ class Common(Configuration):
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
     )
     CORS_ORIGIN_ALLOW_ALL = True
 
